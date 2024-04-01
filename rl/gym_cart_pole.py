beea01558ec41c6e19dfe1712d684a0fe62077b0
Steven Ly <stevenly1000@gmail.com>
2024-04-01 18:35:53 +0200

    Commit testfile RL

---
+++ rl/gym_cart_pole.py
@@ -0,0 +1,19 @@
+import random
+import gym
+
+env = gym.make('CartPole-v0')
+env.reset()
+
+episodes = 10
+
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = random.choice([0,1])
+        n_state, reward, done, info = env.step(action)
+        score+=reward
+    print('Episode:{} Score:{}'.format(episode, score))
