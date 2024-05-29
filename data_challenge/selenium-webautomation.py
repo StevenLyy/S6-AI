f1940e852e4bfe37386f4f2bd262364933e7615b
StevenLyy <stevenly1000@gmail.com>
2024-05-29 14:52:17 +0200

    DC - Datetime function to obtain weekly/monthly data

--- data_challenge/selenium-webautomation.py
+++ data_challenge/selenium-webautomation.py
@@ -1,3 +1,5 @@
+import time
+
 from security.decrypter import decrypt
 from datetime import date, timedelta
 from selenium import webdriver
@@ -15,6 +17,7 @@ website_url = "https://mijn.sitedish.nl/"
 driver = webdriver.Chrome(options=chrome_options)
 wait = WebDriverWait(driver, 7)
 
+
 def login():
     username = driver.find_element(By.NAME, 'username')
     password = driver.find_element(By.NAME, 'password')
@@ -25,58 +28,37 @@ def login():
     login_button = driver.find_element(By.XPATH, '/html/body/form/div/button')
     login_button.click()
 
+
 def navigate_to_dishes():
     wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/a'))).click()
     wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div[6]/a'))).click()
 
-def enter_dates():
-    wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input[1]'))).send_keys("01-2-2024")
-    wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input[3]'))).send_keys("29-2-2024")
+
+def enter_dates(start_date, end_date, type):
+    wait.until(EC.visibility_of_element_located(
+        (By.XPATH, '/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input[1]')))
+    if type == "thuisbezorgd":
+
+    elif type == "POS":
+
+    else:
+        return ValueError("Unknown type")
+
+    start_date_field = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input[1]')
+    end_date_field = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input[3]')
+
+    current_date = start_date
+    while current_date <= end_date:
+        start_date_field.send_keys(current_date.strftime("%d-%m-%Y"))
+        current_date += timedelta(days=7)
+        end_date_field.send_keys(current_date.strftime("%d-%m-%Y"))
+        start_date_field.clear()
+        end_date_field.clear()
+
 
 if __name__ == "__main__":
     driver.get(website_url)
     login()
     navigate_to_dishes()
-    enter_dates()
-
-
-
-
-
-
-
-
-
-
-
-# def enter_and_export_dates(date_from, date_to):
-# # Starting and end dates
-# start_date = date(2021, 7, 1)
-# end_date = date(2024, 4, 30)
-#   # Find the date input fields (replace with actual locators)
-#   date_from_field = driver.find_element(By.ID, "date_from")
-#   date_to_field = driver.find_element(By.ID, "date_to")
-#
-#   # Clear any existing values
-#   date_from_field.clear()
-#   date_to_field.clear()
-#
-#   # Enter the dates as YYYY-MM-DD format
-#   date_from_field.send_keys(date_from.strftime('%Y-%m-%d'))
-#   date_to_field.send_keys(date_to.strftime('%Y-%m-%d'))
-#
-#   # Find the export button and click it
-#   export_button = driver.find_element(By.ID, export_button_locator)
-#   export_button.click()
-#
-# current_date = start_date
-# while current_date <= end_date:
-#   enter_and_export_dates(current_date, current_date)
-#   current_date += timedelta(days=1)
-
-
-
-
-
-
+    enter_dates(start_date=date(2021, 7, 1), end_date=date(2024, 4, 30))
 
