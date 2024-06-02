bce96b571b45386ba7b03dcf992304954e328005
Steven Ly <stevenly1000@gmail.com>
2024-06-02 14:34:22 +0200

    DC - Set selenium download directory to curr GIT repo + 4s download wait

--- data_challenge/selenium-webautomation.py
+++ data_challenge/selenium-webautomation.py
@@ -12,10 +12,16 @@ from selenium.webdriver.common.keys import Keys
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-download_dir = "C:/Users/Beven/Downloads"
-# prefs = {"download.default_directory": download_dir}
-# chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("--start-maximized")
+
+download_dir = os.path.join(os.getcwd(), "data")
+prefs = {
+    "download.default_directory": download_dir,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+}
+chrome_options.add_experimental_option("prefs", prefs)
 
 website_url = "https://mijn.sitedish.nl/"
 
@@ -23,12 +29,12 @@ driver = webdriver.Chrome(options=chrome_options)
 wait = WebDriverWait(driver, 7)
 
 
-def login():
+def login(key_file, encrypted_file):
     username = driver.find_element(By.NAME, 'username')
     password = driver.find_element(By.NAME, 'password')
 
-    username.send_keys(decrypt("key.key", "vxvxvxv.txt")[0])
-    password.send_keys(decrypt("key.key", "vxvxvxv.txt")[1])
+    username.send_keys(decrypt(key_file, encrypted_file)[0])
+    password.send_keys(decrypt(key_file, encrypted_file)[1])
 
     login_button = driver.find_element(By.XPATH, '/html/body/form/div/button')
     login_button.click()
@@ -54,13 +60,15 @@ def enter_dates_and_export(start_date, end_date, system):
 
     current_date = start_date
     while current_date <= end_date:
+        print(current_date, current_date + timedelta(days=7))
+        new_file_name = f"{system}__{current_date}__{current_date + timedelta(days=7)}.csv"
         start_date_field.send_keys(current_date.strftime("%d-%m-%Y"))
         current_date += timedelta(days=7)
         end_date_field.send_keys(current_date.strftime("%d-%m-%Y"))
         driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[1]/a[1]').click()
-        time.sleep(2)
+        time.sleep(4)
+
         downloaded_file = max([download_dir + "/" + f for f in os.listdir(download_dir)], key=os.path.getctime)
-        new_file_name = f"{system}_{current_date}_{current_date}.csv"
         new_file_path = os.path.join(download_dir, new_file_name)
 
         os.rename(downloaded_file, new_file_path)
@@ -70,6 +78,6 @@ def enter_dates_and_export(start_date, end_date, system):
 
 if __name__ == "__main__":
     driver.get(website_url)
-    login()
+    login("key.key", "vxvxvxv.txt")
     navigate_to_dishes()
-    enter_dates_and_export(start_date=date(2021, 7, 1), end_date=date(2024, 4, 30), system="POS")
+    enter_dates_and_export(start_date=date(2021, 7, 1), end_date=date(2024, 4, 30), system="Thuisbezorgd")
